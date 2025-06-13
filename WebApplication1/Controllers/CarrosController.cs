c0ed93c7e9992ed24267856159c3e4fd4be3f5b4
Aluno <espindola.caio@gmail.com>
2025-06-13 20:47:13 -0300

    adicionando arquivos

---
+++ WebApplication1/Controllers/CarrosController.cs
@@ -0,0 +1,46 @@
+﻿using AluguelDeCarros.Data;
+
+using AluguelDeCarrosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AluguelDeCarros.Controllers
+{
+    public class CarrosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CarrosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("Carros")]
+        public IActionResult Index()
+        {
+            var carros = _context.Carros.ToList();
+            return View(carros);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Carro carro)
+        {
+            _context.Carros.Add(carro);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        [Route("Carros/Detalhes/{id}")]
+        public IActionResult Detalhes (int id)
+        {
+            var carros = _context.Carros.ToList().Where(x => x.Id == id).FirstOrDefault();
+            return View(carros);
+
+        }
+    }
+}
